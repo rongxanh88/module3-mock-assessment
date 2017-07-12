6e35ff8a29da202566d1de11b042414a32b3f8d3
rongxanh88 <rongxanh88@gmail.com>
2017-07-12 10:51:09 -0600

    finish refactoring testing

--- spec/features/search_house_members_for_colorado_spec.rb
+++ spec/features/search_house_members_for_colorado_spec.rb
@@ -2,22 +2,19 @@ require "rails_helper"
 
 RSpec.feature "Search House Members", :type => :feature do
   scenario "User searches for house members from Colorado" do
-    # 7.times do |n|
-    #   create(:representative, name: "Rep#{n}", repid: n, district: n)
-    # end
-
     visit root_path
 
     select "Colorado", :from => "state"
     click_on "Locate Members of the House"
+    representatives = Representative.where(state: "CO").order("seniority DESC")
 
-    expect(page).to have_current_path(search_path)
     expect(page).to have_content("7 Results")
+    7.times do |n|
+      expect(page).to have_content(representatives[n-1].name)
+      expect(page).to have_content(representatives[n-1].role)
+      expect(page).to have_content(representatives[n-1].party)
+      expect(page).to have_content(representatives[n-1].seniority)
+      expect(page).to have_content(representatives[n-1].district)
+    end
   end
 end
-
-# Then my path should be "/search" with "state=CO" in the parameters
-# And I should see a message "7 Results"
-# And I should see a list of 7 the members of the house for Colorado
-# And they should be ordered by seniority from most to least
-# And I should see a name, role, party, and district for each member
